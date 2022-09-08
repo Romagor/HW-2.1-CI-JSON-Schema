9cc9ea022c13df47e52f779d8f6074922789c324
RomaGor <goryachevskii60@gmail.com>
2022-09-08 18:53:28 +0300

    feat: modify tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -1,7 +1,6 @@
 package ru.netology.rest;
 
 import io.restassured.http.ContentType;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,18 +10,19 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV5 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .contentType(ContentType.JSON)
-          .body("every{ it.balance >= 0 }", is(true))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("every{ it.balance >= 0 }", is(true))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
